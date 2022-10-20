58ff7cf42cfb0aa1be363fd5267507ff1e108e29
Tomás Donoso <39070152+Koda-Kode@users.noreply.github.com>
2022-10-20 10:08:55 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,44 @@
+function primeros() {
+    var x = document.getElementById("primeros");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function segundos() {
+    var x = document.getElementById("segundos");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function terceros() {
+    var x = document.getElementById("terceros");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function cuartos() {
+    var x = document.getElementById("cuartos");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function quintos() {
+    var x = document.getElementById("quintos");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
