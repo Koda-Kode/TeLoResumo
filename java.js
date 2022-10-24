4687852428fbec809d0d05a50da8d5730a543b88
Tomás Donoso <tomaseportillo@gmail.com>
2022-10-24 11:33:44 -0300

    responsive

--- java.js
+++ java.js
@@ -1,5 +1,14 @@
   // Mostrar el desplegable de los cursos
+function desplegar(){
+  var desplegable = document.getElementById("cursos");
 
+    if (desplegable.style.display == "none") {
+      desplegable.style.display = "block";
+    } else {
+      desplegable.style.display = "none";
+    }
+
+}
 
   // Desplegar cada curso
 function cursos(curso) {
